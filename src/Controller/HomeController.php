e85d4dfc096bd7dfc3b42b671a2ee8efda33cebb
dev-talbi <dev.talbi@gmail.com>
2021-02-03 22:31:08 +0100

    create home, add like with ajax

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Injustice;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,8 +14,12 @@ class HomeController extends AbstractController
      */
     public function index(): Response
     {
+        $injustices = $this->getDoctrine()
+            ->getRepository(Injustice::class)->findAll();
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'injustices' => $injustices,
         ]);
     }
 }
