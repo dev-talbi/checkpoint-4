106ac1bac5beae1b666a0156bb3a59021e987721
dev-talbi <dev.talbi@gmail.com>
2021-02-04 16:34:52 +0100

    css home and show one

--- src/Form/InjusticeType.php
+++ src/Form/InjusticeType.php
@@ -2,18 +2,13 @@
 
 namespace App\Form;
 
-use App\Entity\User;
 use App\Entity\Injustice;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
-use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 
 
@@ -24,8 +19,6 @@ class InjusticeType extends AbstractType
 
     public function __construct(Security $security)
     {
-        // Avoid calling getUser() in the constructor: auth may not
-        // be complete yet. Instead, store the entire Security object.
         $this->security = $security;
     }
 
@@ -42,13 +35,11 @@ class InjusticeType extends AbstractType
                 'choice_label'    => 'pseudo',
                 'multiple' => false,
                 'data' => $user
-
             ), [
                 'attr' => ['class' => 'hider'],
             ])
             ->add('picture')
             ->add('theme');
-        var_dump($user->getPseudo());
     }
 
     public function configureOptions(OptionsResolver $resolver)
